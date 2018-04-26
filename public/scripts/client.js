8954b8b066d4e30ab70bbc439369b6f364a1f133
Alex Hand <alex.j.hand@gmail.com>
2018-04-26 14:37:19 -0500

    Imported React and set up basic component

---
+++ public/scripts/client.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>LFC Schedule</h1>
+            <br />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
