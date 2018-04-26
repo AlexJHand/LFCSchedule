77c45e9d5c1c3806cb4f3cfece2f33b770ddd26a
Alex Hand <alex.j.hand@gmail.com>
2018-04-26 14:40:16 -0500

    Index router created

---
+++ routes/index.router.js
@@ -0,0 +1,13 @@
+// Requires
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// Get index
+router.get('/', function (req, res) {
+    console.log('Request for index');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Exports
+module.exports = router;
