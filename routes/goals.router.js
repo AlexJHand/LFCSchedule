19141fbec5a205260d2071f0938b3d7dd9c636bf
Alex Hand <alex.j.hand@gmail.com>
2018-05-13 20:15:05 -0500

    Exports

--- routes/goals.router.js
+++ routes/goals.router.js
@@ -3,3 +3,6 @@ const router = require('express').Router();
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
+
+// Exports 
+module.exports = router;
