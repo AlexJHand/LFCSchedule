04ee1e74c55d6e50f77b4abcfea2e96211d9fb71
Alex Hand <alex.j.hand@gmail.com>
2018-07-24 17:08:38 -0500

    Added card header

--- client/src/App.js
+++ client/src/App.js
@@ -118,6 +118,7 @@ class App extends Component {
             <div className="headerTitle">Liverpool FC Dashboard</div>
           </div>
           <div className="card">
+            <div className="matchesHeader">Upcoming Matches</div>
             <div className="matches">
               <div key={list.matches[0].objId} className="match1 matchCard">
                 <div className="matchOneImages matchImages">
