dae03a46089b9ca27c565e6678554d58fc363df2
Alex Hand <alex.j.hand@gmail.com>
2018-08-08 13:33:51 -0500

    heroku

--- routes/index.router.js
+++ routes/index.router.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const path = require('path');
 
 // Get index
-router.get('/', function (req, res) {
-    console.log('Request for index');
-    // res.sendFile(path.join(__dirname, '../public/index.html'));
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    // "proxy": "http://localhost:4501/",
+// router.get('/', function (req, res) {
+//     console.log('Request for index');
+//     // res.sendFile(path.join(__dirname, '../public/index.html'));
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//     // "proxy": "http://localhost:4501/",
+// });
+
+router.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Exports
