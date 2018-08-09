643cfda32111806fb0d8986940b85bf0a4e5f6a9
Alex Hand <alex.j.hand@gmail.com>
2018-08-09 15:43:39 -0500

    Heroku

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const port = 4501;
 
 // Require Routers
 const goalsRouter = require('./routes/goals.router')
-const matchesRouter = require('./routes/matches.router.js');
+const matchesRouter = require('./routes/matches.router');
 const tableRouter = require('./routes/table.router');
 const indexRouter = require('./routes/index.router');
 app.use(express.static(path.join(__dirname, './client/build')));
 
-// app.get('*', (request, response) => {
-//     response.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 // Use Routers
 app.use('/goals', goalsRouter);
