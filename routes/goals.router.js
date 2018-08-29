5d423ac432a2e8ea369c12f79f7189333d5b271f
Alex Hand <alex.j.hand@gmail.com>
2018-08-28 20:34:25 -0500

    Assists and CleanSheets classes created

--- routes/goals.router.js
+++ routes/goals.router.js
@@ -4,11 +4,33 @@ const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
-class ScorersClass {
-    constructor(name, team, goals) {
+class GoalsClass {
+    constructor(rank, name, team, nationality, goals) {
+        this.rank = rank,
         this.name = name,
         this.team = team,
+        this.nationality = nationality,
         this.goals = goals
+    }
+}
+
+class AssistsClass {
+    constructor(rank, name, team, nationality, assists) {
+        this.rank = rank,
+        this.name = name,
+        this.team = team,
+        this.nationality = nationality,
+        this.assists = assists
+    }
+}
+
+class CleanSheetsClass {
+    constructor(rank, name, team, nationality, cleanSheets) {
+        this.rank = rank,
+        this.name = name,
+        this.team = team,
+        this.nationality = nationality,
+        this.cleanSheets = cleanSheets
     }
 }
 
