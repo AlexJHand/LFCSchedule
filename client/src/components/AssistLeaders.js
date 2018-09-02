1dca8fde7d2dc7480116315d4fc443350558e6b8
Alex Hand <alex.j.hand@gmail.com>
2018-09-02 16:35:53 -0500

    Created AssistLeaders component

---
+++ client/src/components/AssistLeaders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class AssistLeaders extends React.Component {
+    render() {
+        return (
+            <tr className="assistLeader">
+                <td className="assistsRank">{this.props.rank}</td>
+                <td className="assistsName">{this.props.name}</td>
+                <td className="assistsTeam">{this.props.team}</td>
+                <td className="assistsScored">{this.props.assists}</td>
+            </tr>
+        )
+    }
+}
