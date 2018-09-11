57c7261427abe2418d0190571abfcca6f56e3d3d
Alex Hand <alex.j.hand@gmail.com>
2018-09-11 11:49:57 -0500

    Created Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+    render () {
+        return (
+            <div className="Footer">
+                <p className="copyright"></p>
+                <p className="contact"></p>
+            </div>
+        )
+    }
+}
